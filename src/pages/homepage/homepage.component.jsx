57abff50bc5b8a7a293bdf4336a553a624ba3585
codycc <linkmecody@gmail.com>
2022-03-15 14:47:08 -0400

    restructure code

--- src/pages/homepage/homepage.component.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import Directory from '../../components/directory/directory.component';
-
-import './homepage.styles.scss';
-const HomePage = () => (
-    <Directory />
-)
-
-export default HomePage;
